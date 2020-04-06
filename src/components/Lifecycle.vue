793f3410fb6d7e1d3016444ad3d9e0db8bb74d5f
Sam Arbid <sam.arbid@capcito.com>
2020-04-06 23:44:10 +0200

    composition api

---
+++ src/components/Lifecycle.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Life cycle</h1>
+    </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted } from 'vue'
+export default {
+    setup(){
+       onMounted(() => {
+            console.log(`Component is now mounted `);
+        })
+       onUnmounted(() => {
+            console.log(`Component is Unmounted `);
+        })
+        }
+    }
+</script>
---
+++ src/components/MousePosition.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>mouse data</h1>
+        <h2>x = {{ x }}</h2>
+        <h2>y = {{ y }}</h2>
+    </div>
+</template>
+<script>
+import { useMousePosition } from '../Tools/mouse.js'
+export default {
+    setup(){
+        const {x,y} = useMousePosition()
+        
+        return{
+            x,
+            y
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
