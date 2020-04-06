256c28189142a7bb0ad18643b7f04bf99c337b22
Sam Arbid <sam.arbid@capcito.com>
2020-04-06 22:41:40 +0200

    watcher and watch effect

--- src/components/Watch.vue
+++ src/components/Watch.vue
@@ -3,12 +3,18 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref , watchEffect, watch } from 'vue'
 
 export default {
   setup() {
     const number = ref(0)
-    setInterval(() => { number.value += 1},500)
+    setInterval(() => { number.value += 1},1000)
+    watchEffect(() => console.log(`find a change ${number.value} times!`)
+    )
+    watch(number,(number, previous) => {
+      console.log(`changed from ${previous} to ${number}`);
+      
+    })
     return {
       number,
     }
